4235b862b2a217d91dd2b4f4343784ac864aea62
RupanGuvi <rupanguvi@gmail.com>
2024-10-07 20:39:17 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddTodo from './Components/AddTodo';
+import Todos from './Components/Todos';
 
 const App = () => {
   return (
     <div>
       <h1>App Component</h1>
+      <AddTodo />
+      <Todos />
     </div>
   );
 };
--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeTodo } from "../Features/Slice";
 
 const Todos = () => {
-    return (
-        <div>
-        <h1>Todo's</h1>
-        </div>
-    );
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+
+  return (
+    <div>
+      <h1>Todo's</h1>
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li 
+          className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+          key={todo.id}>
+            <div className="text-white">{todo.text}</div>
+            <button 
+            onClick={() => dispatch(removeTodo(todo.id))}
+            className='text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'
+            >
+              Remove Todo
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Todos;
