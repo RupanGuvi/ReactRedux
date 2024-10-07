41ca7c98317628b248be502f1ebb27cb0b173152
RupanGuvi <rupanguvi@gmail.com>
2024-10-07 19:25:46 +0530

    redux part completed

--- src/Features/Slice.jsx
+++ src/Features/Slice.jsx
@@ -1,11 +1,26 @@
-import {createSlice , nanoid} from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-       todos:[{id:1,text:"Hello World"}]
-}
+  todos: [{ id: 1, text: "Hello World" }],
+};
 
 export const Slice = createSlice({
-    name: 'todo',
-    initialState,
-    reducers:{}
-})
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = Slice.actions;
+
+export default Slice.reducer;
--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,6 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit";
-
+import todoReducer from "../Features/Slice"
 
 export const store = configureStore({
-    reducer:{}
+    reducer: todoReducer
 })
